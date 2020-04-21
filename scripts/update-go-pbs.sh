349b301bbe44b8bea8c2bf2afe8c2e4e8630168a
Raul Jordan <raul@prysmaticlabs.com>
2020-04-21 15:03:00 -0500

    Fix Go Update PB Script to Include Generated SSZ (#134)
    * resolve
    
    * update script to include generated ssz
    
    * confs

--- scripts/update-go-pbs.sh
+++ scripts/update-go-pbs.sh
@@ -11,16 +11,33 @@ file_list=()
 while IFS= read -d $'\0' -r file ; do
     file_list=("${file_list[@]}" "$file")
 done < <(find -L $(bazel info bazel-bin)/eth -type f -name "*pb.go" -print0)
- 
+
 arraylength=${#file_list[@]}
 searchstring="/ethereumapis/"
 
 # Copy pb.go files from bazel-bin to original folder where .proto is.
-
 for (( i=0; i<${arraylength}; i++ ));
 do
   destination=${file_list[i]#*$searchstring}
+  echo ${destination}
   cp -R -L "${file_list[i]}" "$destination"
   echo Updated $destination
 done
 
+# Copy the generated.ssz.go file to live in the same package as 
+# the generated protos.
+file_list=()
+while IFS= read -d $'\0' -r file ; do
+    file_list=("${file_list[@]}" "$file")
+done < <(find -L $(bazel info bazel-bin)/eth -type f -name "*ssz.go" -print0)
+
+arraylength=${#file_list[@]}
+searchstring="/bin/"
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+  destination=${file_list[i]#*$searchstring}
+  echo ${destination}
+  cp -R -L "${file_list[i]}" "$destination"
+  echo Updated $destination
+done
