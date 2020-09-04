da7ad52ee814719491ea22673fb1b6921bf75bf3
Victor Farazdagi <simple.square@gmail.com>
2020-09-04 20:49:23 +0300

    asserts that gfind is available on OSX, for protobufs updates

--- scripts/update-go-pbs.sh
+++ scripts/update-go-pbs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. $(dirname "$0")/common.sh
 
 # Script to copy pb.go files from bazel build folder to appropriate location.
 # Bazel builds to bazel-bin/... folder, script copies them back to original folder where .proto is.
@@ -8,36 +9,34 @@ bazel query 'kind(go_library, //...) union kind(go_proto_library, //...)' | xarg
 # Get locations of pb.go files.
 
 file_list=()
-while IFS= read -d $'\0' -r file ; do
+while IFS= read -d $'\0' -r file; do
     file_list=("${file_list[@]}" "$file")
-done < <(find -L $(bazel info bazel-bin)/eth -type f -regextype sed -regex ".*pb\.\(gw\.\)\?go$" -print0)
+done < <($findutil -L $(bazel info bazel-bin)/eth -type f -regextype sed -regex ".*pb\.\(gw\.\)\?go$" -print0)
 
 arraylength=${#file_list[@]}
 searchstring="/ethereumapis/"
 
 # Copy pb.go files from bazel-bin to original folder where .proto is.
-for (( i=0; i<${arraylength}; i++ ));
-do
-  destination=${file_list[i]#*$searchstring}
-  echo ${destination}
-  cp -R -L "${file_list[i]}" "$destination"
-  echo Updated $destination
+for ((i = 0; i < ${arraylength}; i++)); do
+    destination=${file_list[i]#*$searchstring}
+    color "34" $destination
+    cp -R -L "${file_list[i]}" "$destination"
+    echo Updated $destination
 done
 
-# Copy the generated.ssz.go file to live in the same package as 
+# Copy the generated.ssz.go file to live in the same package as
 # the generated protos.
 file_list=()
-while IFS= read -d $'\0' -r file ; do
+while IFS= read -d $'\0' -r file; do
     file_list=("${file_list[@]}" "$file")
-done < <(find -L $(bazel info bazel-bin)/eth -type f -name "*ssz.go" -print0)
+done < <($findutil -L $(bazel info bazel-bin)/eth -type f -name "*ssz.go" -print0)
 
 arraylength=${#file_list[@]}
 searchstring="/bin/"
 
-for (( i=0; i<${arraylength}; i++ ));
-do
-  destination=${file_list[i]#*$searchstring}
-  echo ${destination}
-  cp -R -L "${file_list[i]}" "$destination"
-  echo Updated $destination
+for ((i = 0; i < ${arraylength}; i++)); do
+    destination=${file_list[i]#*$searchstring}
+    color "34" $destination
+    cp -R -L "${file_list[i]}" "$destination"
+    echo Updated $destination
 done
