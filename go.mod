daf15b50b2dc9d87522d9c03cdf8e54cb3da97fe
Ivan Martinez <ivanthegreatdev@gmail.com>
2020-12-12 21:21:13 -0500

    Add v1lpha1

--- go.mod
+++ go.mod
@@ -7,13 +7,14 @@ require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.4.3
 	github.com/grpc-ecosystem/grpc-gateway v1.13.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.0.1 // indirect
 	github.com/jhump/protoreflect v1.8.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.0 // indirect
 	github.com/prysmaticlabs/go-bitfield v0.0.0-20200322041314-62c2aee71669
 	github.com/prysmaticlabs/protoc-gen-go-cast v0.0.0-20201210230745-1451ec2916a9 // indirect
 	golang.org/x/tools v0.0.0-20201210181237-6d345e82f1d8 // indirect
-	google.golang.org/genproto v0.0.0-20200528191852-705c0b31589b
-	google.golang.org/grpc v1.29.1
+	google.golang.org/genproto v0.0.0-20201019141844-1ed22bb0c154
+	google.golang.org/grpc v1.33.1
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.0.1 // indirect
 	google.golang.org/protobuf v1.25.1-0.20200805231151-a709e31e5d12
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- scripts/run-go-cast.sh
+++ scripts/run-go-cast.sh
@@ -4,7 +4,7 @@ ethV1Files=$(ls eth/v1/*.proto)
 for protoFile in $ethV1Files
 do
   protoc -I. -Ibazel-ethereumapis/external/go_googleapis/ -Ibazel-ethereumapis/$ethV1Path -Ibazel-ethereumapis/external/com_google_protobuf/src --experimental_allow_proto3_optional "$protoFile" --go-cast_out=$ethV1Path
-  protoc -I. -Ibazel-ethereumapis/external/go_googleapis/ -Ibazel-ethereumapis/$ethV1Path -Ibazel-ethereumapis/external/com_google_protobuf/src --experimental_allow_proto3_optional --go-grpc_out $ethV1GatewayPath "$protoFile"
+  protoc -I. -Ibazel-ethereumapis/external/go_googleapis/ -Ibazel-ethereumapis/$ethV1Path -Ibazel-ethereumapis/external/com_google_protobuf/src --experimental_allow_proto3_optional --grpc-gateway_out $ethV1GatewayPath "$protoFile"
 done
 
 ethV1PBFiles=$(ls eth/v1/github.com/prysmaticlabs/ethereumapis/eth/v1/*.pb.go)
@@ -25,6 +25,35 @@ rm -rf eth/v1/github.com/
 rm -rf eth/v1_gateway/github.com/
 
 bazel build //eth/v1:go_default_library
+
+cp "bazel-out/k8-fastbuild/bin/${ethV1Path}generated.ssz.go" "$ethV1Path/generated.ssz.go"
+
+ethV1Path=eth/v1alpha1/
+ethV1GatewayPath=eth/v1alpha1_gateway/
+ethV1Files=$(ls eth/v1alpha1/*.proto)
+for protoFile in $ethV1Files
+do
+  protoc -I. -Ibazel-ethereumapis/external/go_googleapis/ -Ibazel-ethereumapis/$ethV1Path -Ibazel-ethereumapis/external/com_google_protobuf/src --experimental_allow_proto3_optional "$protoFile" --go-cast_out=$ethV1Path
+done
+
+ethV1PBFiles=$(ls eth/v1alpha1/github.com/prysmaticlabs/ethereumapis/eth/v1alpha1/*.pb.go)
+for pbGoFile in $ethV1PBFiles
+do
+  filename="${pbGoFile##*/}"
+  mv "$pbGoFile" "$ethV1Path$filename"
+done
+
+ethV1PBFiles=$(ls eth/v1_gateway/github.com/prysmaticlabs/ethereumapis/eth/v1alpha1/*.pb.go)
+for pbGoFile in $ethV1PBFiles
+do
+  filename="${pbGoFile##*/}"
+  mv "$pbGoFile" "$ethV1GatewayPath$filename"
+done
+
+rm -rf eth/v1alpha1/github.com/
+rm -rf eth/v1alpha1_gateway/github.com/
+
+bazel build //eth/v1alpha1:go_default_library
 
 cp "bazel-out/k8-fastbuild/bin/${ethV1Path}generated.ssz.go" "$ethV1Path/generated.ssz.go"
 
