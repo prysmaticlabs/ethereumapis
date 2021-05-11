7ef8f431075a7c10ab11412774ffa24eaaa13660
Atif Anowar <atif@lukso.io>
2021-05-11 11:45:17 +0200

    Fix api defition

--- eth/v1alpha1_gateway/beacon_chain.pb.gw.go
+++ eth/v1alpha1_gateway/beacon_chain.pb.gw.go
@@ -748,20 +748,9 @@ func local_request_BeaconChain_NextEpochProposerList_0(ctx context.Context, mars
 
 }
 
-var (
-	filter_BeaconChain_StreamNewPendingBlocks_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_BeaconChain_StreamNewPendingBlocks_0(ctx context.Context, marshaler runtime.Marshaler, client BeaconChainClient, req *http.Request, pathParams map[string]string) (BeaconChain_StreamNewPendingBlocksClient, runtime.ServerMetadata, error) {
-	var protoReq StreamPendingBlocksRequest
+	var protoReq empty.Empty
 	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BeaconChain_StreamNewPendingBlocks_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 
 	stream, err := client.StreamNewPendingBlocks(ctx, &protoReq)
 	if err != nil {
